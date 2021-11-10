bdbbc4d7f27404f9cb955d3885da5523840acd05
Mikołaj Lenczewski <mikolaj.lenczewski308@gmail.com>
2021-11-10 19:33:01 +0000

    hyperman: updated stdlib commit

--- main_node_main.c
+++ main_node_main.c
@@ -4,6 +4,10 @@
 
 #include "imxrt_pins.h"
 
+void delay(u32 cycles) {
+	for (volatile u32 i = 0; i < cycles; i++);
+}
+
 int main() {
 	CORE_PIN13_CONFIG = 5;
 	CORE_PIN13_PADCONFIG = IOMUXC_PAD_DSE(7);
@@ -11,10 +15,12 @@ int main() {
 
 	while (1) {
 		CORE_PIN13_PORTSET = CORE_PIN13_BITMASK;
-		stdlib_delay_msec(500);
+//		stdlib_delay_msec(1000);
+		delay(10000000);
 
 		CORE_PIN13_PORTCLEAR = CORE_PIN13_BITMASK;
-		stdlib_delay_msec(500);
+//		stdlib_delay_msec(1000);
+		delay(10000000);
 	}
 
 	return 0;
